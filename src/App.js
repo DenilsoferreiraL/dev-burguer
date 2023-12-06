38037987a5868466b66aec03b382dadc3f7465ed
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2023-12-06 09:53:17 -0300

    Primeira Versão da Página Inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react"
+import {
+    Container,
+    ContainerItens,
+    Image,
+    H1,
+    Label,
+    Input,
+    Button
 
+} from "./styles"
 
-const App = () => {
+import logo from "./assets/logo-burguer.svg"
 
+function App() {
 
+    return (
+        <Container>
+            <Image alt="logo-imagem" src={logo} />
+            <ContainerItens>
+
+                <H1>Faça seu pedido!</H1>
+                <Label>Pedido</Label>
+                <Input></Input>
+
+                <Label>Nome do Cliente</Label>
+                <Input></Input>
+
+                <Button>Novo Pedido</Button>
+            </ContainerItens>
+        </Container>
+
+    )
 }
+
 
 export default App
---
+++ src/styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+background-color: black;
+background-size: cover;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 40px;
+height: 100%;
+min-height: 100vh;
+`
+
+export const Image = styled.img`
+max-width:342px;
+width: 100%;
+height: auto;
+`
+
+export const H1 = styled.p`
+color: #ffffff;
+font-size: 28px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`
+export const ContainerItens = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const Label = styled.label`
+color:#ffffff;
+`
+
+export const Input = styled.input`
+
+`
+
+export const Div = styled.button`
+
+`
+
+export const Button = styled.button`
+
+`
+
